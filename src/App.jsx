bf89d495e749d6cecd8ea98aa024d879e4707787
Tushar Suryawanshi <159779529+iamtushar28@users.noreply.github.com>
2025-04-21 11:53:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div className="py-2 md:py-8 absolute top-0 z-[-2] h-auto w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
 
         {/* heading */}
-        <h1 className='text-3xl md:text-5xl font-bold text-white text-center mt-4'>🧑‍⚕️ Medical Services</h1>
+        <h1 className='text-3xl md:text-5xl font-bold text-white text-center mt-4'>React CRUD operation 🛠️</h1>
 
         <MedicalServiceForm />
 
